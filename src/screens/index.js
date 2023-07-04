1f55314f33e047b837e457bbff36a93fb7adc38a
devgaurav1 <104262681+devgaurav1@users.noreply.github.com>
2023-07-05 01:26:21 +0530

    Update index.js

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,4 +2,3 @@ export {default as Search} from './search';
 export {default as Gift} from './gift';
 export {default as Shop} from './shop';
 export {default as Home} from './home';
-// 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;
